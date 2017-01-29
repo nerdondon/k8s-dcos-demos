0bff6642b6838bc7106f293e5db77586685dd851
nerdondon <willdon@nerdondon.com>
2017-01-29 03:09:07 -0800

    Add some error handling

--- dotnet-test-app/Controllers/HomeController.cs
+++ dotnet-test-app/Controllers/HomeController.cs
@@ -12,8 +12,8 @@ namespace dotnet_test_app.Controllers
         {
             var podNodeName = Environment.GetEnvironmentVariable("POD_NODE_NAME");
             var podName = Environment.GetEnvironmentVariable("POD_NAME");
-            ViewData["POD_NODE_NAME"] = podNodeName;
-            ViewData["POD_NAME"] = podName;
+            ViewData["POD_NODE_NAME"] = podNodeName ?? "node name not found";
+            ViewData["POD_NAME"] = podName ?? "pod name not found";
             return View();
         }
         
