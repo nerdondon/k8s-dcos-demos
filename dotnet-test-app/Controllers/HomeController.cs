996f6842b6ec01bacbb6db31e25f7dc9ad4e07fe
nerdondon <willdon@nerdondon.com>
2017-03-05 03:57:12 -0800

    Create a new v2 of app to have a joke for presentation

--- dotnet-test-app/Controllers/HomeController.cs
+++ dotnet-test-app/Controllers/HomeController.cs
@@ -14,14 +14,6 @@ namespace dotnet_test_app.Controllers
             var podName = Environment.GetEnvironmentVariable("POD_NAME");
             ViewData["POD_NODE_NAME"] = podNodeName ?? "node name not found";
             ViewData["POD_NAME"] = podName ?? "pod name not found";
-            var x = 0.0001;
-            //500000
-            long iterations = 5000000;
-            for(long i = 0; i <= iterations; i++) {
-                x += Math.Sqrt(x);
-            }
-            ViewData["CALC"] = x;
-            ViewData["ITERATIONS"] = iterations;
             return View();
         }
         
